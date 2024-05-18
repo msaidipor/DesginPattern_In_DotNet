483201575a84abf4989e68078e0039966c153ffb
Mohsen <m.saidipor@gmail.com>
2024-05-19 00:38:06 +0330

    add Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+Console.WriteLine("Singleton");
+
+SingletonClass s1 = SingletonClass.Instance;
+
+SingletonClass s2 = SingletonClass.Instance;
+
+Console.WriteLine(s1.Id == s2.Id);
+
+
+Console.ReadKey();
---
+++ Singleton/SingletonPattern.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class SingletonClass
+    {
+        private static readonly object lockObject = new object();
+        private static SingletonClass instance = null;
+
+        public int Id;
+        public SingletonClass()
+        {
+                Id=Random.Shared.Next();
+        }
+
+
+        public static SingletonClass Instance
+        {
+            get
+            {
+                lock(lockObject)
+                {
+                    if (instance == null)  instance = new SingletonClass();
+                    return instance;
+                    
+                }
+
+                //for test
+                //return new SingletonClass();
+            }
+        }
+    }
+}
