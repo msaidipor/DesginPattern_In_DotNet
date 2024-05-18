83d29d9a8de7d837645a04fbc9d51f36ea05a0b7
Mohsen <m.saidipor@gmail.com>
2024-05-18 12:38:33 +0330

    Add ProtoType

---
+++ ProtoType/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ProtoType;
+
+Employee lissa = new Employee { Name = "Lissa", Age = 50, Department = new Department { Name = "IT" } };
+Employee lissaClone = (Employee)lissa.Clone(false);
+
+lissaClone.Department.Name = "HR";
+
+Console.WriteLine("Deep Clone");
+Console.WriteLine(lissa.Department.Name);
+Console.WriteLine(lissaClone.Department.Name);
+
+Employee anna = new Employee { Name = "Anna", Age = 20, Department = new Department { Name = "IT" } };
+Employee annaClone = (Employee)anna.Clone(true);
+
+annaClone.Department.Name = "HR";
+
+Console.WriteLine("Shallow Clone");
+Console.WriteLine(anna.Department.Name);
+Console.WriteLine(annaClone.Department.Name);
+
+Console.ReadKey();
---
+++ ProtoType/ProtoTypePattern.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoType
+{
+
+    public interface ICloneableObject
+    {
+        object Clone(bool isShallow);
+       
+    }
+    public class Employee : ICloneableObject
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Department Department { get; set; }
+        public object Clone(bool isShallow)
+        {
+           
+
+            return isShallow ? ShallowCopy() : DeepCopy();
+            
+        }
+
+        private object ShallowCopy()
+        {
+            return MemberwiseClone();
+        }
+
+        private object DeepCopy()
+        {
+            Employee clone = (Employee)MemberwiseClone();
+            clone.Department = (Department)Department.Clone();
+            return clone;
+        }
+    }
+
+    public class Department : ICloneable
+    {
+        public string Name { get; set; }
+
+        public object Clone()
+        {
+
+            return MemberwiseClone();
+
+        }
+    }
+}
