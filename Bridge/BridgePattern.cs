e1d552cbd402c70682e86975bbd7f37dd58c2a11
Mohsen <m.saidipor@gmail.com>
2024-05-22 00:31:39 +0330

    add Bridge

---
+++ Bridge/BridgePattern.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+
+    /// <summary>
+    /// The 'Implementor' interface
+    /// </summary>
+    public interface Implementor
+    {
+        string OperationImplementor();
+    }
+
+    /// <summary>
+    /// The 'Abstraction' class
+    /// </summary>
+    public class Abstraction(Implementor implementor)
+    {
+        public virtual string Operation()
+        {
+            return implementor.OperationImplementor();
+        }
+    }
+
+    /// <summary>
+    /// The 'RefinedAbstraction' class
+    /// </summary>
+    public class RefinedAbstraction(Implementor implementor) : Abstraction(implementor)
+    {
+       
+        public override string Operation()
+        {
+            
+            return "Refined Abstraction " + base.Operation();
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteImplementor' class
+    /// </summary>
+
+    public class ConcreteImplementorA : Implementor
+    {
+        public string OperationImplementor()
+        {
+            return "ConcreteImplentorA";
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteImplementor' class
+    /// </summary>
+    public class ConcreteImplementorB : Implementor
+    {
+        public string OperationImplementor()
+        {
+            return "ConcreteImplentorB";
+        }
+    }
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bridge;
+
+Console.WriteLine("Bridge");
+
+Abstraction abstraction =new Abstraction(new ConcreteImplementorA());
+
+Console.WriteLine(abstraction.Operation());
+abstraction=new RefinedAbstraction(new ConcreteImplementorB());
+Console.WriteLine(abstraction.Operation());
+
+Console.ReadKey();
