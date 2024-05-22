f6f3643d203969c4da34bb05aca338cffc46c287
Mohsen <m.saidipor@gmail.com>
2024-05-22 17:31:40 +0330

    add Composite

---
+++ Composite/CompositePattern.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Composite
+{
+
+    /// <summary>
+    /// Component
+    /// </summary>
+    public  abstract class FileComponent
+    {
+        public string Name { get; set; }
+        public FileComponent(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Add(FileComponent component)
+        {
+            Console.WriteLine("add");
+        }
+
+        public virtual void Remove(FileComponent component)
+        {
+            Console.WriteLine("remove");
+        }
+
+        public virtual void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + Name);
+        }
+    }
+
+    /// <summary>
+    /// composite
+    /// </summary>
+    public class DirectoryComposite(string name) : FileComponent(name)
+    {
+        private List<FileComponent> _children = new List<FileComponent>();
+
+      
+
+        public override void Add(FileComponent component)
+        {
+            _children.Add(component);
+        }
+
+        public override void Remove(FileComponent component)
+        {
+            _children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + "Directory: " + Name);
+            foreach (FileComponent component in _children)
+            {
+                component.Display(depth + 2);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// leaf
+    /// </summary>
+    public class FileLeaf(string name) : FileComponent(name)
+    {
+       
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + "File: " + Name);
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Composite;
+
+Console.WriteLine("composite");
+
+
+FileComponent rootDirectory = new DirectoryComposite("RootDirectory");
+FileComponent file1 = new FileLeaf("File1.txt");
+FileComponent subDirectory1 = new DirectoryComposite("SubDirectory1");
+FileComponent file2 = new FileLeaf("File2.txt");
+FileComponent file3 = new FileLeaf("File3.txt");
+
+rootDirectory.Add(file1);
+rootDirectory.Add(subDirectory1);
+subDirectory1.Add(file2);
+subDirectory1.Add(file3);
+
+rootDirectory.Display(2);
+subDirectory1.Display(2);
+
+Console.ReadKey();
