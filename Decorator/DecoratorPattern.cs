b5073889fe9d0360934b06c079925a0bbbc59f5a
Mohsen <m.saidipor@gmail.com>
2024-05-25 02:54:23 +0330

    add Decorator

---
+++ Decorator/DecoratorPattern.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+
+    /// <summary>
+    /// Base Component Interfase
+    /// </summary>
+  public interface IRoom
+    {
+        string GetDescription();
+        int GetPrice();
+    }
+
+
+    /// <summary>
+    /// Concrete Component
+    /// </summary>
+    public class SimpleRoom : IRoom
+    {
+        public string GetDescription()
+        {
+            return "Base Room";
+        }
+
+        public int GetPrice()
+        {
+            return 1;
+        }
+    }
+
+    /// <summary>
+    /// Base Decorator Class
+    /// </summary>
+    public abstract class RoomDecorator(IRoom _room): IRoom
+    {
+
+        public virtual string GetDescription()
+        {
+            if (_room != null)
+              return  _room.GetDescription();
+            else return "oioi";
+        }
+
+        public virtual int GetPrice()
+        {
+            if (_room != null)
+              return  _room.GetPrice();
+            else return 1;
+        }
+    }
+
+    /// <summary>
+    /// Concrete Decorator
+    /// </summary>
+    /// <param name="_room"></param>
+    public class WifiDecorator(IRoom _room) : RoomDecorator(_room)
+    {
+        public override string GetDescription()
+        {
+            return base.GetDescription() + " + Wifi";
+        }
+
+        public override int GetPrice()
+        {
+            return base.GetPrice() + 1;
+        }
+    }
+
+    /// <summary>
+    /// Concrete Decorator
+    /// </summary>
+    /// <param name="_room"></param>
+    public class BreakfastDecorator(IRoom _room) : RoomDecorator(_room)
+    {
+        public override string GetDescription()
+        {
+            return base.GetDescription() + " + Breakfast";
+        }
+
+        public override int GetPrice()
+        {
+            return base.GetPrice() + 2;
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator;
+
+Console.WriteLine("Decorator");
+
+
+SimpleRoom room = new SimpleRoom();
+
+Console.WriteLine(room.GetDescription());
+
+WifiDecorator wifiDecorator = new WifiDecorator(room);
+Console.WriteLine(wifiDecorator.GetDescription());
+
+BreakfastDecorator breakfastDecorator = new BreakfastDecorator(room);
+Console.WriteLine(breakfastDecorator.GetDescription());
+
+breakfastDecorator=new BreakfastDecorator(wifiDecorator);
+Console.WriteLine(breakfastDecorator.GetDescription());
+
+Console.ReadKey();
