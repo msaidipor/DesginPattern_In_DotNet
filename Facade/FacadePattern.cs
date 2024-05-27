6266397864aca145618b344cb49315b46f4fb38f
Mohsen <m.saidipor@gmail.com>
2024-05-27 12:51:03 +0330

    Add Facade

---
+++ Facade/FacadePattern.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+
+    /// <summary>
+    /// Facade Interface
+    /// </summary>
+    public interface IOrderService
+    {
+        void ProccessOrder();
+    }
+
+    /// <summary>
+    /// Facade Class
+    /// </summary>
+    public class OrderService(Product product,Payment payment,Invoice invoice) : IOrderService
+    {
+      
+        public void ProccessOrder()
+        {
+            product.GetProductDetails();
+            payment.MakePayment();
+            invoice.Sendinvoice();
+        }
+    }
+
+    public class Product
+    {
+        public void GetProductDetails()
+        {
+            Console.WriteLine("Fetching the Product Details");
+        }
+    }
+
+    public class Payment
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Done Successfully");
+        }
+    }
+
+    public class Invoice
+    {
+        public void Sendinvoice()
+        {
+            Console.WriteLine("Invoice Send Successfully");
+        }
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Facade;
+
+Console.WriteLine("Facade");
+
+IOrderService orderService = new OrderService(new Product(),new Payment(),new Invoice());
+orderService.ProccessOrder();
+
+Console.ReadKey();
