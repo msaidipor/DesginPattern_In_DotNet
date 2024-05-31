78c7564bf48a03121fe8eb7992ad6b63452b34a5
Mohsen <m.saidipor@gmail.com>
2024-05-31 20:03:50 +0330

    add flyweight

---
+++ FlyWeight/FlyweightPattern.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Flyweight
+{
+
+
+    /// <summary>
+    /// Flyweight Interface
+    /// </summary>
+   public interface IFont
+    {
+        void SetSize(int size);
+        void SetStyle(string style);
+        void SetColor(string color);
+        void Display(string text);
+    }
+
+
+    /// <summary>
+    /// Flywieght Factory Class
+    /// </summary>
+    public class FontFactory
+    {
+        private Dictionary<string, IFont> flyweights = new Dictionary<string, IFont>();
+
+
+        public IFont GetFont(string key)
+        {
+            if (!flyweights.ContainsKey(key))
+            {
+                flyweights[key] = new ConcretaFlyweight();
+            }
+            return flyweights[key];
+        }
+    }
+
+
+    /// <summary>
+    /// Flyweight Concrete Class
+    /// </summary>
+
+    public class ConcretaFlyweight : IFont
+    {
+        private int size;
+        private string style;
+        private string color;
+
+        public void SetSize(int size)
+        {
+            this.size = size;
+        }
+
+        public void SetStyle(string style)
+        {
+            this.style = style;
+        }
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+
+        public void Display(string text)
+        {
+            Console.WriteLine($"Text: '{text}' | Size: {size} | Style: {style} | Color: {color}");
+        }
+    }
+
+   
+}
---
+++ FlyWeight/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Flyweight;
+
+Console.WriteLine("Flyweight");
+
+
+FontFactory fontFactory = new FontFactory{
+
+};
+
+// Get shared flyweight objects
+IFont font1 = fontFactory.GetFont("Arial");
+IFont font2 = fontFactory.GetFont("Times New Roman");
+IFont font3 = fontFactory.GetFont("Arial");
+
+// Set intrinsic state
+font1.SetSize(12);
+font1.SetStyle("Regular");
+font1.SetColor("Black");
+
+font2.SetSize(14);
+font2.SetStyle("Italic");
+font2.SetColor("Red");
+
+// Use flyweight objects
+font1.Display("Hello, Flyweight Pattern!");
+font2.Display("Hello, Flyweight Pattern!");
+font3.Display("Hello, Flyweight Pattern!");
+
+Console.ReadKey();
