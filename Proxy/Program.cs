b781c63154d152425c77d9ae9d56f0b1c9be0550
Mohsen <m.saidipor@gmail.com>
2024-06-02 12:14:52 +0330

    add Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Proxy;
+
+Console.WriteLine("Proxy");
+
+IDatabase database = new ProxyDatabase(new Database());
+
+Console.WriteLine(database.GetData());
+Console.WriteLine(database.GetData());
+
+
+Console.ReadKey();
---
+++ Proxy/ProxyPattern.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+
+
+    /// <summary>
+    /// The "Subject" Interface
+    /// </summary>
+    public interface IDatabase
+    {
+        string GetData();
+    }
+
+    /// <summary>
+    /// The "RealSubject" Class
+    /// </summary>
+    public class Database : IDatabase
+    {
+        public string GetData()
+        {
+            return "Data From Database";
+        }
+    }
+
+
+    /// <summary>
+    /// The "Proxy" Class
+    /// </summary>
+    public class ProxyDatabase : IDatabase
+    {
+        private readonly Database _database=new();
+        private string _cacheData;
+        public ProxyDatabase(Database database)
+        {
+                _database=database;
+        }
+
+        public string GetData()
+        {
+            if (string.IsNullOrEmpty(_cacheData)) _cacheData = _database.GetData();
+            else _cacheData = "cache:"+ _cacheData;
+            
+            return _cacheData;
+        }
+    }
+
+
+    
+}
